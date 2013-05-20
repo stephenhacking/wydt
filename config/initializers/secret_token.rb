1f795830b19b96464b616b9e02f8902ec429b1aa
Ashwin <ashwin@organicapex.com>
2013-05-20 15:56:13 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wydt::Application.config.secret_token = 'ec68fca5ca85f227c129aa423d1f93c2bdc4e189e931103e48cb3f54a91e086086c62a84322ffdc61be12bd6b0658c2f83f158106ecd6ace97d78912da15de71'
